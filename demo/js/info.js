95fb6b891ffd65db906199e51f70db5042d2d896
zawedx <1071022348@qq.com>
2022-01-18 23:15:07 +0800

    initialize info

--- demo/js/info.js
+++ demo/js/info.js
@@ -8,5 +8,5 @@ function update_and_renew(from_date, to_date, editors, times){
     $("#t3").text("到 " + info_data[1])
     $("#t4").text("共 " + info_data[2] + " 个编辑者参与编辑了 " + info_data[3] + " 次")
 }
-// update_and_renew("2020-01-03 03:48", "2020-12-29 16:40", "451", "3821")
+update_and_renew("2020-01-19 02:17", "2020-12-31 00:19", "326", "11566")
 // update_and_renew("2020-01-03 03:48", "2020-12-29 16:40", "451", "3821")
